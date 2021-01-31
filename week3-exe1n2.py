8c5e15c7006983b2cf9ab11c946b784958e6eaf0
root <root@DESKTOP-DNEOKH2.localdomain>
2021-01-31 10:28:19 -0500

    Week 3 Exercise 1 and 2

---
+++ week3-exe1n2.py
@@ -0,0 +1,6 @@
+exercise = [{'ip_addr':'10.220.88.1','mac_addr':'0062.ec29.70fe','interface':'Gi0/0/0'},
+                   {'ip_addr':'10.220.88.20','mac_addr':'c89c.1dea.0eb6','interface':'Gi0/0/0'}]
+devices = [{'hostname':'srx2.lasthop.io','username':'test','password':'test123'},
+           {'hostname':'arista4.lasthop.io','username':'test','password':'test123'}]
+print(exercise[0])
+print(devices[1]['hostname'])
